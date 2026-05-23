4c8d635597afaefbf691d04b3934287e8721fe48
kantero-tech <hassandayisaba@gmail.com>
2026-05-23 17:38:24 +0200

    fix: allow db init with empty appId at build time to unblock SSG

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,8 @@
 import { init } from '@instantdb/react';
 import schema from './schema';
 
-const appId = process.env.NEXT_PUBLIC_INSTANTDB_APP_ID;
-
-if (!appId) {
-  throw new Error('NEXT_PUBLIC_INSTANTDB_APP_ID is not set');
-}
-
-export const db = init({ appId, schema });
+// Falls back to '' at build time (SSG); real value must be set via env var at runtime
+export const db = init({
+  appId: process.env.NEXT_PUBLIC_INSTANTDB_APP_ID ?? '',
+  schema,
+});
